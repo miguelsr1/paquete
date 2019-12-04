9d3c58d6aa580226f6df6abd532fda0d6eacd39a
Tadeo Rivera-Pineda <xtecuan@gmail.com>
2019-12-04 07:59:11 -0600

    pre produccion

--- boleta-ejb/src/main/java/sv/gob/mined/boleta/ejb/LeerBoletasEJB.java
+++ boleta-ejb/src/main/java/sv/gob/mined/boleta/ejb/LeerBoletasEJB.java
@@ -1,14 +1,124 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sv.gob.mined.boleta.ejb;
 
-/**
- *
- * @author misanchez
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.mail.Session;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.multipdf.Splitter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+@Stateless
+@LocalBean
 public class LeerBoletasEJB {
-    
+
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("parametros");
+
+    @EJB
+    private EMailEJB eMailEJB;
+
+    public void leerArchivosPendientes(Session mailSession, String codDepa) {
+        Properties info = chargeEmailsProperties("emails0212");
+
+        File carpetaRoot = new File(RESOURCE_BUNDLE.getString("path_archivo"));
+        for (File carpetaDepa : carpetaRoot.listFiles()) {
+            if (carpetaDepa.isDirectory()) {
+                for (File carpetaPorFecha : carpetaDepa.listFiles()) {
+                    if (carpetaPorFecha.isDirectory()) {
+                        for (File archivoBoleta : carpetaPorFecha.listFiles()) {
+                            if (archivoBoleta.isFile()) {
+                                splitPages(archivoBoleta, codDepa, mailSession, info);
+                                Logger.getLogger(LeerBoletasEJB.class.getName()).log(Level.INFO, archivoBoleta.getName());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void splitPages(File file, String codDepa, Session mailSession, Properties info) {
+        PDDocument document = null;
+        try {
+            //try (InputStream is = new FileInputStream(file)) {
+            document = PDDocument.load(file);
+
+            Splitter splitter = new Splitter();
+
+            List<PDDocument> lstPages = splitter.split(document);
+
+            int i = 1;
+            for (PDDocument pd : splitter.split(document)) {
+                String code = getCodigoDocente(pd, "         )", 0, 15).substring(8);
+                if (info.containsKey(code)) {
+                    String email = info.getProperty(code);
+
+                    //enviarMail(code, email, pd);
+                    //Logger.getLogger(LeerBoletasEJB.class.getName()).log(Level.INFO, "Envio numero: {0} - codigo departamento {1} - codigo docente {2}", new Object[]{i, codDepa, code});
+                    
+                    //i++;
+                }
+            }
+
+
+            //is.close();
+            document.close();
+            // }
+        } catch (IOException ex) {
+            try {
+                if (document != null) {
+                    document.close();
+                }
+
+            } catch (IOException ex1) {
+                Logger.getLogger(LeerBoletasEJB.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+
+            Logger.getLogger(LeerBoletasEJB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Properties chargeEmailsProperties(String nombre) {
+        Properties info = null;
+        try {
+            info = new Properties();
+            try (InputStream input = LeerBoletasEJB.class.getClassLoader().getResourceAsStream(nombre + ".properties")) {
+                info.load(input);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(LeerBoletasEJB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return info;
+    }
+
+    public static String getCodigoDocente(PDDocument pDDocument, String strEndIdentifier, int offSet, int back) {
+        String returnString;
+        try {
+            PDFTextStripper tStripper = new PDFTextStripper();
+            tStripper.setStartPage(1);
+            tStripper.setEndPage(1);
+            String pdfFileInText = tStripper.getText(pDDocument);
+            String strEnd = strEndIdentifier;
+            int endInddex = pdfFileInText.indexOf(strEnd) + offSet;
+            int startInddex = endInddex - back;
+            returnString = pdfFileInText.substring(startInddex, endInddex); // + strEnd;
+        } catch (IOException ex) {
+            returnString = "No ParaGraph Found";
+        }
+        return returnString;
+    }
+
 }
--- boleta-web/src/main/java/sv/gob/mined/boleta/web/BoletaMB.java
+++ boleta-web/src/main/java/sv/gob/mined/boleta/web/BoletaMB.java
@@ -5,14 +5,10 @@
  */
 package sv.gob.mined.boleta.web;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,36 +24,22 @@ import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.activation.DataHandler;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.mail.Address;
 import javax.mail.Authenticator;
-import javax.mail.BodyPart;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.util.ByteArrayDataSource;
 import javax.servlet.ServletContext;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.pdfbox.io.MemoryUsageSetting;
-import org.apache.pdfbox.multipdf.Splitter;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.primefaces.PrimeFaces;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 import sv.gob.mined.boleta.dto.DatosDto;
+import sv.gob.mined.boleta.ejb.LeerBoletasEJB;
 import sv.gob.mined.utils.jsf.JsfUtil;
 
 /**
@@ -68,24 +50,26 @@ import sv.gob.mined.utils.jsf.JsfUtil;
 @ViewScoped
 public class BoletaMB implements Serializable {
 
-    private Boolean visible = true;
-    private String usuario, clave, codDepa, mesAnho;
+    private String usuario;
+    private String clave;
+    private String codDepa;
+    private String mesAnho;
     private Date fecha;
     private SimpleDateFormat sdf = new SimpleDateFormat("MM_yyyy");
     private List<DatosDto> lstPendientes = new ArrayList();
     private List<DatosDto> lstProcesados = new ArrayList();
 
     private UploadedFile file;
-    private PDDocument document;
-    private List<PDDocument> lstPages;
-    private Iterator<PDDocument> iterator;
 
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("config");
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("parametros");
 
     private Session mailSession;
 
     private Properties config = new Properties();
     private Properties configEmail = new Properties();
+
+    @EJB
+    private LeerBoletasEJB leerBoletasEJB;
 
     @PostConstruct
     public void init() {
@@ -118,11 +102,9 @@ public class BoletaMB implements Serializable {
                             clave);
                 }
             });
-
         } else {
             try {
                 JsfUtil.limpiarVariableSession();
-
                 context.getExternalContext().getSessionMap().clear();
                 ExternalContext externalContext = context.getExternalContext();
                 externalContext.redirect(((ServletContext) externalContext.getContext()).getContextPath() + "/index.mined");
@@ -133,53 +115,29 @@ public class BoletaMB implements Serializable {
         }
     }
 
+    public Properties chargeEmailsProperties(String nombre) {
+        Properties info = null;
+        try {
+            info = new Properties();
+            try (InputStream input = LeerBoletasEJB.class.getClassLoader().getResourceAsStream(nombre + ".properties")) {
+                info.load(input);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(LeerBoletasEJB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return info;
+    }
+
     public void cargarArchivos() throws IOException {
         mesAnho = sdf.format(fecha);
         lstPendientes.clear();
         lstProcesados.clear();
-        File carpetaPendientes = new File("/opt/soporte/" + codDepa + "/" + mesAnho);
+        File carpetaPendientes = new File(RESOURCE_BUNDLE.getString("path_archivo") + File.separator + codDepa + File.separator + mesAnho);
         cargarDatosDeArchivos(lstPendientes, carpetaPendientes);
-        /*if (carpetaPendientes.exists()) {
-            for (File listFile : carpetaPendientes.listFiles()) {
-                if (listFile.isFile()) {
-                    DatosDto dato = new DatosDto();
 
-                    Path pathFile = listFile.toPath();
-                    FileTime fTime = (FileTime) Files.getAttribute(pathFile, "creationTime");
-                    PDDocument pdc = PDDocument.load(listFile);
-
-                    dato.setFechaCreado(new Date(fTime.toMillis()));
-                    dato.setNombreArchivo(listFile.getName());
-                    dato.setNumeroPaginas(pdc.getNumberOfPages());
-
-                    lstPendientes.add(dato);
-                }
-            }
-        } else {
-            carpetaPendientes.mkdir();
-        }*/
-
-        File carpetaProcesados = new File("/opt/soporte/" + codDepa + "/" + mesAnho + "/procesado/");
+        File carpetaProcesados = new File(RESOURCE_BUNDLE.getString("path_archivo") + File.separator + codDepa + File.separator + mesAnho + File.separator + "procesado" + File.separator);
         cargarDatosDeArchivos(lstProcesados, carpetaProcesados);
-        /*if (carpetaProcesados.exists()) {
-            for (File listFile : carpetaProcesados.listFiles()) {
-                if (listFile.isFile()) {
-                    DatosDto dato = new DatosDto();
-
-                    Path pathFile = listFile.toPath();
-                    FileTime fTime = (FileTime) Files.getAttribute(pathFile, "creationTime");
-                    PDDocument pdc = PDDocument.load(listFile);
-
-                    dato.setFechaCreado(new Date(fTime.toMillis()));
-                    dato.setNombreArchivo(listFile.getName());
-                    dato.setNumeroPaginas(pdc.getNumberOfPages());
-
-                    lstProcesados.add(dato);
-                }
-            }
-        } else {
-            carpetaProcesados.mkdir();
-        }*/
     }
 
     private void cargarDatosDeArchivos(List<DatosDto> lst, File carpeta) throws IOException {
@@ -197,6 +155,8 @@ public class BoletaMB implements Serializable {
                     dato.setNumeroPaginas(pdc.getNumberOfPages());
 
                     lst.add(dato);
+                    
+                    pdc.close();
                 }
             }
         } else {
@@ -212,10 +172,6 @@ public class BoletaMB implements Serializable {
         this.fecha = fecha;
     }
 
-    public Boolean getVisible() {
-        return visible;
-    }
-
     public UploadedFile getFile() {
         return file;
     }
@@ -228,12 +184,12 @@ public class BoletaMB implements Serializable {
         this.file = file.getFile();
         JsfUtil.mensajeInformacion("El archivo esta en cola para ser procesado. Al finalizar se enviara un correo notificando el envío de las boletas.");
 
-        File carpeta = new File("/opt/soporte/" + codDepa + "/" + mesAnho + "/");
+        File carpeta = new File(RESOURCE_BUNDLE.getString("path_archivo") + File.separator + codDepa + File.separator + mesAnho + File.separator);
         if (!carpeta.exists()) {
             carpeta.mkdir();
         }
 
-        Path folder = Paths.get("/opt/soporte/" + codDepa + "/" + mesAnho);
+        Path folder = Paths.get(RESOURCE_BUNDLE.getString("path_archivo") + File.separator + codDepa + File.separator + mesAnho);
         String filename = FilenameUtils.getBaseName(file.getFile().getFileName());
         String extension = FilenameUtils.getExtension(file.getFile().getFileName());
         Path arc = Files.createTempFile(folder, filename + "-", "." + extension);
@@ -241,7 +197,8 @@ public class BoletaMB implements Serializable {
         try (InputStream input = file.getFile().getInputstream()) {
             Files.copy(input, arc, StandardCopyOption.REPLACE_EXISTING);
         }
-        visible = false;
+
+        cargarArchivos();
     }
 
     public List<DatosDto> getLstPendientes() {
@@ -251,100 +208,15 @@ public class BoletaMB implements Serializable {
     public List<DatosDto> getLstProcesados() {
         return lstProcesados;
     }
-
-    public void upload() {
-        if (file != null) {
-            splitPages();
-            file = null;
-
-            document = null;
-        }
-    }
-
-    public void splitPages() {
-        try {
-            InputStream is = file.getInputstream();
-            document = PDDocument.load(is, MemoryUsageSetting.setupTempFileOnly());
-
-            Splitter splitter = new Splitter();
-
-            lstPages = splitter.split(document);
-
-            //Creating an iterator 
-            iterator = lstPages.listIterator();
-
-            final Properties info = chargeEmailsProperties("emails0212");
-            int i = 1;
-
-            while (iterator.hasNext()) {
-                DatosDto rowData = new DatosDto();
-
-                PDDocument pd = iterator.next();
-
-                String code = getCode2(pd, "         )", 0, 15).substring(8);
-                if (info.containsKey(code)) {
-                    String email = info.getProperty(code);
-                    /*rowData.setCodigo(code);
-                    rowData.setCorreoElectronico(email);*/
-
-                    //enviarMail(code, email, pd);
-                    lstPendientes.add(rowData);
-                    PrimeFaces.current().ajax().update("tblDatos");
-                    System.out.println("envio " + i);
-                }
-                pd.close();
-
-                i++;
-            }
-
-            document.close();
-            iterator.remove();
-            is.close();
-        } catch (IOException ex) {
-            try {
-                document.close();
-
-            } catch (IOException ex1) {
-                Logger.getLogger(BoletaMB.class.getName()).log(Level.SEVERE, null, ex1);
-            }
-
-            Logger.getLogger(BoletaMB.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public Properties chargeEmailsProperties(String nombre) {
-        Properties info = null;
-        try {
-            info = new Properties();
-            InputStream input = BoletaMB.class
-                    .getClassLoader().getResourceAsStream(nombre + ".properties");
-            //fin            
-            info.load(input);
-            input.close();
-
-        } catch (IOException ex) {
-            Logger.getLogger(BoletaMB.class
-                    .getName()).log(Level.SEVERE, null, ex);
-        }
-        return info;
-    }
-
-    public static String getCode2(PDDocument pDDocument, String strEndIdentifier, int offSet, int back) {
-        String returnString;
-        try {
-            PDFTextStripper tStripper = new PDFTextStripper();
-            tStripper.setStartPage(1);
-            tStripper.setEndPage(1);
-            String pdfFileInText = tStripper.getText(pDDocument);
-            String strEnd = strEndIdentifier;
-            int endInddex = pdfFileInText.indexOf(strEnd) + offSet;
-            int startInddex = endInddex - back;
-            returnString = pdfFileInText.substring(startInddex, endInddex); // + strEnd;
-        } catch (IOException ex) {
-            returnString = "No ParaGraph Found";
-        }
-        return returnString;
-    }
+//
+//    public void upload() {
+//        if (file != null) {
+//            splitPages();
+//            file = null;
+//
+//            document = null;
+//        }
+//    }
 
 //    //@Asynchronous
 //    public void enviarMail(String code, String remitente, PDDocument pDDocument) throws IOException {
@@ -401,4 +273,7 @@ public class BoletaMB implements Serializable {
 //            Logger.getLogger(BoletaMB.class.getName()).log(Level.INFO, "Error en el envio de correo", ex);
 //        }
 //    }
+    public void verificarArchivosPendientes() {
+        leerBoletasEJB.leerArchivosPendientes(mailSession, codDepa);
+    }
 }
